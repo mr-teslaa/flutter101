63bf2b1b0094c611f3fca6f8195a865006c64012
mr-teslaa <mr.teslaa@protonmail.com>
2022-01-15 01:12:31 +0600

    lesson finished

--- README.md
+++ README.md
@@ -1,44 +1,27 @@
 ### Goals of this lesson
 
--   Image assets
-    -   `Cloud Hosted Image`: For accessing any image from cloud we need to use the syntext like this
-    ```dart
-    body: const Center(
-            child: Image(
-                image: NetworkImage('imageURL'),
-            ),
-        ),
+-   Buttons
+
+    -   `Raised Button`: this button will have some shadow, looks like it just raised from ground
+        _NB: Updated version of flutter is no longer support this type of version. We are more prefered to use `ElevatedButton` instead_
+
+    -   We can also use colors to change the color
+
+    -   `FlatButton`: this is another kind of button which doesn't have any shadow
+        _NB: Updated version of flutter is no longer support this type of version. We are more prefered to use `ElevatedButton` instead_
+
+    -   `icon with button`: if we want an icon inside the button we could access them both like this `ElevatedButton.icon(....)`
+
+    -   `icon button`: if we want to use only icon as a button, then the sytex will be
+
     ```
-    **Short code for Network/Cloud hosted image is**
+    IconButton(
+        onPressed: () { },
+        icon: Icon(Icons.alternate_email),
+        color: Colors.black,
+    )
     ```
-    body: const Center(
-            child: Image.network(imageURL),
-        ),
-    ```
-    -   `Local hosted image`: For accessing locally avaiable image, our syntex will be like,
-    ```dart
-    body: const Center(
-            child: Image(
-                image: AssetImage('assets/coffee.jpg'),
-            ),
-        ),
-    ```
-    **Short code for Local Hosted image is**
-    ```
-    body: const Center(
-            child: Image.assets(imageURL),
-        ),
-    ```
-    _NB: befor you use local hosted image we need to edit the pubspec.yaml file. For enabling images we neeed to configure the .yaml file like this_
-    ```
-    assets:
-        - assets/coffee.jpg
-    ```
-    _which means we have an image called `coffee.jpg` in our assets folder, we can now access that perticular file. But if we want to access 1000 of images what should we do then? well in that case we can simply say that_
-    ```
-    assets:
-        - assets/
-    ```
-    _which means whatever image hosted in `assets` folder I need to use them in my app so enable them, and yeah that's it_
+
+**Remeber to use `onPressed` argument, otherwise the button will not work. Syntext is `onPressed: () {...}`**
 
 # to be continued in the next chapter
--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,75 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  MaterialApp(
-      home: Home(),
-  ),
-);
-
+      MaterialApp(
+        home: Home(),
+      ),
+    );
 
 class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Salam App'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
+      appBar: AppBar(
+        title: const Text('Salam App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        // ===== expample of icon =====
+        // child: Icon(
+        //   Icons.airport_shuttle,
+        //   color: Colors.lightBlue,
+        //   size: 100,
+        // ),
+
+        // ===== button without icon =====
+        // child: ElevatedButton(
+        //   onPressed: () {},
+        //   child: const Text(
+        //     'I am a button',
+        //     style: TextStyle(
+        //       color: Colors.black,
+        //     ),
+        //   ),
+        //   style: ElevatedButton.styleFrom(
+        //     primary: Colors.yellow,
+        //   ),
+        // )
+
+        // ===== button with icon  =====
+        // child: ElevatedButton.icon(
+        //   onPressed: () {},
+        //   icon: Icon(
+        //     Icons.mail,
+        //     color: Colors.black
+        //   ),
+        //   label: Text(
+        //     'Mail me',
+        //     style: TextStyle(
+        //       color: Colors.black,
+        //     ),
+        //   ),
+        //   style: ElevatedButton.styleFrom(
+        //     primary: Colors.yellow,
+        //   ),
+        // )
+
+        //  ===== icon button =====
+        child: IconButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.black,
         ),
-        body: Center(
-          child: Image.asset('assets/coffee.jpg')
-          // for accessing network image 
-          // child: Image.network('imageURL')
-        ),
-        floatingActionButton: FloatingActionButton(
+      ),
+      floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text('click'),
-          backgroundColor: Colors.red[600]
-        ),
-      );
+          backgroundColor: Colors.red[600]),
+    );
   }
 }
